ca290f86da56bf085af9a2c6fbf3b7ec23b52f99
Jonas Macedo <jonasmdcarvalho@gmail.com>
2019-09-27 22:27:12 -0300

    Criando componentes.

---
+++ front_end/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './style';
+
+export default function Header() {
+  return (
+    <Container>
+      
+    </Container>
+  );
+}
--- front_end/src/pages/Main/main.js
+++ front_end/src/pages/Main/main.js
@@ -1,12 +1,58 @@
 import React from 'react';
 
+import './style.css';
+
 export default function Main() {
 
     return(
         <div className = "main-container">
-            <img alt="Imagedo do produto"></img>
+            <ul>
+                <img alt="logoEmpresa"></img>
+          
+                <li>
+                    <img alt="Imagedo do produto"></img>
+                    <footer>
+                        <strong>Nome do produto</strong>
+                        <p>informações do produto</p>
+                    </footer>
+                    <div className="button-compra">
+                        <img alt="ImageBotamCompra"></img>
+                    </div>
+                </li>
 
-            <p>teste da rota nova!</p>
+
+                <li>
+                    <img alt="Imagedo do produto"></img>
+                    <footer>
+                        <strong>Nome do produto</strong>
+                        <p>informações do produto</p>
+                    </footer>
+                    <div className="button-compra">
+                        <img alt="ImageBotamCompra"></img>
+                    </div>
+                </li>
+                <li>
+                    <img alt="Imagedo do produto"></img>
+                    <footer>
+                        <strong>Nome do produto</strong>
+                        <p>informações do produto</p>
+                    </footer>
+                    <div className="button-compra">
+                        <img alt="ImageBotamCompra"></img>
+                    </div>
+                </li>
+                <li>
+                    <img alt="Imagedo do produto"></img>
+                    <footer>
+                        <strong>Nome do produto</strong>
+                        <p>informações do produto</p>
+                    </footer>
+                    <div className="button-compra">
+                        <img alt="ImageBotamCompra"></img>
+                    </div>
+                </li>
+            </ul>
         </div>
+          
     )
 }
